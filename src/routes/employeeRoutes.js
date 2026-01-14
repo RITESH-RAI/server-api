e711525f24473bab5d7216077fde36f85e6c1768
Utkarsh Pandey <utk2665@gmail.com>
2026-01-14 19:10:12 +0530

    All REST APIs

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -2,7 +2,21 @@ const express = require("express");
 const router = express.Router();
 const Employee = require("../models/employee");
 
-router.post("/emp", async (req, res) => {
+
+//TODO: GET all employees
+router.get("/employee-list", async (req, res) => {
+  try {
+    const employees = await Employee.find({}, { name: 1});
+
+    res.status(200).json(employees);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error (GET)",
+    });
+  }
+});
+
+router.post("/employee-create", async (req, res) => {
   try {
     const { name, password, address } = req.body;
 
@@ -30,4 +44,63 @@ router.post("/emp", async (req, res) => {
   }
 });
 
+//TODO: PUT - Update Employees
+
+router.put("/employee-update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, password, address } = req.body;
+
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+      id,
+      {
+        name,
+        password,
+        address,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedEmployee) {
+      return res.status(404).json({
+        message: "Employee not found",
+      });
+    }
+
+    res.status(200).json(updatedEmployee);
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error (PUT)",
+      error: error.message
+    });
+  }
+});
+
+//TODO: DELETE Endpoint for Employee deletion
+router.delete("/employee-delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleteEmployee = await Employee.findByIdAndDelete(id);
+
+    if (!deleteEmployee) {
+      return res.status(404).json({
+        message: "Employee not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Employee Deleted Successfully",
+    });
+  } catch (error) {
+
+    res.status(500).json({
+      message: "Server error (DELETE)",
+      error: error.message,
+    })
+  }
+});
 module.exports = router;
